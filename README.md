d3780a043c0123d471352cd98e6022d3c728876c
lenart <lenartlola@proton.me>
2023-06-23 11:04:24 +0200

    Update single byte xor

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Cryptography challenges.
 ## Challenges:
 
 1 - Convert hex to base64.
+
+2 - Fixed XOR
+
+3 - Single-byte XOR cipher
--- SingleByteXOR/main.py
+++ SingleByteXOR/main.py
@@ -3,7 +3,7 @@ import binascii
 
 def print_usage():
     print("This script would decrypt a xored string that has been encoded using one single byte character.")
-    print("Usage: python3 main.py <A hex string to decode> <a character to xor>")
+    print("Usage: python3 main.py <A hex string to decode>")
     sys.exit(1)
 
 
@@ -12,12 +12,11 @@ def decode_string(s):
     for key in range(256):
         decoded = "".join(chr(i ^ key) for i in x)
         if decoded.isprintable():
-            print("Key: ", key, "decoded: ", decoded)
+            print("Key: ", key, "Decoded: ", decoded)
 
 def main():
     if len(sys.argv) != 2:
         print_usage()
-    res = decode_string(sys.argv[1])
-    print("Decode: ", res)
+    decode_string(sys.argv[1])
 
 main()
